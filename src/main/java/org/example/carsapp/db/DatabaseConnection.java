7a7459712620136f3722ef285304a97a8d24296a
Solovey Roman <gasoss221510@gmail.com>
2026-05-26 14:52:17 +0300

    Move database config to environment variables & update gitignore

--- src/main/java/org/example/carsapp/db/DatabaseConnection.java
+++ src/main/java/org/example/carsapp/db/DatabaseConnection.java
@@ -1,16 +1,32 @@
 package org.example.carsapp.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-	private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-	private static final String USER = "postgres";
-	private static final String PASSWORD = "1";
+	private static final Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMissing()
+			.load();
+
+	private static final String URL = getEnv("DB_URL");
+	private static final String USER = getEnv("DB_USER");
+	private static final String PASSWORD = getEnv("DB_PASSWORD");
 
 	public Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+
+	private static String getEnv(String key) {
+		String value = dotenv.get(key);
+
+		if (value == null || value.isBlank()) {
+			throw new IllegalStateException("Не задана переменная окружения: " + key);
+		}
+
+		return value;
 	}
 }
